1a3366791c8d17ca68875c6da888c17901b01031
Romaric BANGA <romaric.banga@aninf.ga>
2024-03-31 21:07:24 +0100

    add h2database

--- src/main/java/ga/banga/statemachine/StatemachineApplication.java
+++ src/main/java/ga/banga/statemachine/StatemachineApplication.java
@@ -1,13 +1,27 @@
 package ga.banga.statemachine;
 
+import ga.banga.statemachine.enumeration.Events;
+import ga.banga.statemachine.enumeration.States;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.statemachine.StateMachine;
 
 @SpringBootApplication
-public class StatemachineApplication {
+public class StatemachineApplication implements CommandLineRunner {
+    @Autowired
+    private StateMachine<States, Events> stateMachine;
 
     public static void main(String[] args) {
         SpringApplication.run(StatemachineApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        stateMachine.sendEvent(Events.E1);
+        stateMachine.sendEvent(Events.E2);
+
+        System.out.println("*************** "+ stateMachine.getState().getId());
+    }
 }
